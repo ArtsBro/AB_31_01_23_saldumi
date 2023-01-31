3e87839d0a0af58f6d965d8ed0bdcc7079673a27
ArtsBro <artsbrokans@gmail.com>
2023-01-31 08:39:38 +0200

    pirma reize

---
+++ AB_31_01_23_saldumi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AB_31_01_23_saldumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
